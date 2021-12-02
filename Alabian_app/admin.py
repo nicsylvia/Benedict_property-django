6b9fbebabf02e9521a82b44f3503b8fa787bae38
nicsylvia <nicsylvia15f@gmail.com>
2021-12-02 10:32:59 -0800

    saved day3 assignment on database,migrations,django-admin

--- Alabian_app/admin.py
+++ Alabian_app/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from Alabian_app.models import *
 
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    prepopulated_field = {'slug': ('name',)}
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_field = {'slug': (' product_name',)}
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_field = {'slug': ('category_name',)}
+
+admin.site.register(ProductCategory)
+
--- Alabian_app/models.py
+++ Alabian_app/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+# this was taken from my django ecommerce database file.
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    password = models.CharField(max_length=11)
+    email = models.EmailField()
+    phone = models.TextField(max_length=11)
+    address = models.TextField()
+    slug = models.SlugField(unique=True)
+
+class Product(models.Model):
+    product_name= models.CharField(max_length=24)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    product_price = models.FloatField()
+    image1 = models.FileField(blank=True, null=True, upload_to='uploads/')
+    image2 = models.FileField(blank=True, null=True, upload_to='uploads/')
+    image3 = models.FileField(blank=True, null=True, upload_to='uploads/')
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Category(models.Model):
+    category_name = models.CharField(max_length=30)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    description = models.TextField()
+    image1 = models.FileField(blank=True, null=True, upload_to='uploads/')
+    image2 = models.FileField(blank=True, null=True, upload_to='uploads/')
+    slug = models.SlugField(unique=True)
+
+class ProductCategory(models.Model):
+    category_name = models.ForeignKey(Category, on_delete=models.CASCADE)
+    product_name = models.ForeignKey(Product, on_delete=models.CASCADE)
+    # category_id= models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+
+
+
+
+
